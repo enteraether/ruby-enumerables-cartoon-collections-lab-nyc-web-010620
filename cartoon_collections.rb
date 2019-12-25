ceb0010ead58f3bdf9325809a4d0659da4021df4
Theresa <thetheresagarcia@gmail.com>
2019-12-25 01:06:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index do |value, index|
+    puts "#{index}: #{value}"
+  # code an argument here
   # Your code here
 end
 
