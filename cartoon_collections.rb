f94e8eb37a663ab797ea1efbaa21dc3516e7b938
Theresa <thetheresagarcia@gmail.com>
2019-12-25 16:29:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  array.any? { |i| i.length > 4}
+  # code an argument here
   # Your code here
 end
 
